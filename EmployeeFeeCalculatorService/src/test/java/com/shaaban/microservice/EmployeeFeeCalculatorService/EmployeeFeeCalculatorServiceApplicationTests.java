4efc63c3d79949a97e4b3bda87f5e0991c38ec83
shaaban Altanany <shaaban.eltanany.ext@orange.com>
2020-04-04 09:47:57 +0200

    init the project

---
+++ EmployeeFeeCalculatorService/src/test/java/com/shaaban/microservice/EmployeeFeeCalculatorService/EmployeeFeeCalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaaban.microservice.EmployeeFeeCalculatorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeFeeCalculatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ EmployeeService/src/test/java/com/shaaban/microservice/EmployeeService/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaaban.microservice.EmployeeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
